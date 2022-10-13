339ee0bbf5ba8704afac675ef05ccd42d6af8c00
Nicholasysf <nicholasysf@gmail.com>
2022-10-14 03:59:56 +0800

    Finished adding modal feature to the script

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-const addButton = documnet.querySelector(#)
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button=>{
+    button.addEventListener('click',() =>{
+        // we select each individual modal over here over the course of datasets of modal
+        const modal = document.querySelector(button.dataset.modalTarget);
+        // we open modal by using the function openModal(modal)
+        openModal(modal);
+    })
+})
+
+function openModal(modal){
+    if(modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+closeModalButtons.forEach(button=>{
+    button.addEventListener('click',() =>{
+        const modal = button.closest('.modal')
+        // we close modal by using the function closeModal(modal)
+        closeModal(modal);
+    })
+})
+
+function closeModal(modal){
+    if(modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+overlay.addEventListener('click',()=>{
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal=>{
+        closeModal(modal);
+    })
+})
